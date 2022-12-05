e2c2c56bb9761bcf04bcd6785c9a0dc5a7f5581e
Aadit Gupta <spacegiant21@gmail.com>
2022-12-04 18:10:07 -0800

    api and html

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
@@ -1,14 +1,30 @@
 package com.nighthawk.spring_portfolio.mvc.calculator;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/calculator")
+@Controller
 public class CalculatorApiController {
 
-    @GetMapping("/{expression}")
+    @GetMapping("/calculator")
+    public String getResult(
+            @RequestParam(name = "expression", required = false, defaultValue = "5 * 4") String expression,
+            Model model) {
+
+        // Returns jsonified result of expression with tokens and everything
+        Calculator newCalc = new Calculator(expression);
+        String result = newCalc.toString();
+        model.addAttribute("result", result);
+
+        return "calculator";
+    }
+
+    @GetMapping("/api/calculator/{expression}")
     public ResponseEntity<String> getResult(@PathVariable String expression) {
 
         // Returns jsonified result of expression with tokens and everything
@@ -16,6 +32,7 @@ public class CalculatorApiController {
         String result = newCalc.toJSON();
         return new ResponseEntity<String>(result, HttpStatus.OK);
 
+        // Bad ID
     }
 
 }
